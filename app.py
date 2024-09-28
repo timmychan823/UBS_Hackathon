557cc6a55cb9d473fc54bb92edff1a0c7d412900
timmychan823 <chanshunhei09@gmail.com>
2024-09-28 10:20:02 +0800

    world-game

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def helloworld():
     return 'Hello World'
+
+@app.route("/wordle-game")
+def wordle_game():
+    value = {
+        "language": language,
+        "company": company,
+        "Itemid": Itemid,
+        "price": price
+    }
+
+    return json.dumps(value)
 
 if __name__ == "__main__":
     app.run()
+
