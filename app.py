f849a1878933657745ecf35e0270f809b409ec43
timmychan823 <chanshunhei09@gmail.com>
2024-09-28 19:51:15 +0800

    dfads

--- app.py
+++ app.py
@@ -18,35 +18,35 @@ def wordle_game():
     wrong_positions = []
     correct = [0,0,0,0,0]
     guess = {"guess": "oater"}        
-    if len(content["guessHistory"])!=0:
-        for i in range(len(content['guessHistory'])):
-            for j in range(5):
-                if content["evaluationHistory"][i][j] == "X":
-                    wrong_positions.append(content["guessHistory"][i][j])
-                elif content["evaluationHistory"][i][j] == "-":
-                    try: 
-                        characters.remove(content["guessHistory"][i][j])
-                    except Exception as e:
-                        continue
-                elif content["evaluationHistory"][i][j] == "O":
-                    correct[j]=content["guessHistory"][i][j]
-        if len(content['guessHistory'])!=5:
-            correct = random.choices(characters, k=5) 
-        else:
-            for j in range(5):
-                if correct[j]==0:
-                    if len(wrong_positions) == 0:
-                        correct[j]="".join(random.choices(wrong_positions,k=1))
-                    else:
-                        correct[j]="".join(random.choices(characters,k=1))
+    # if len(content["guessHistory"])!=0:
+    #     for i in range(len(content['guessHistory'])):
+    #         for j in range(5):
+    #             if content["evaluationHistory"][i][j] == "X":
+    #                 wrong_positions.append(content["guessHistory"][i][j])
+    #             elif content["evaluationHistory"][i][j] == "-":
+    #                 try: 
+    #                     characters.remove(content["guessHistory"][i][j])
+    #                 except Exception as e:
+    #                     continue
+    #             elif content["evaluationHistory"][i][j] == "O":
+    #                 correct[j]=content["guessHistory"][i][j]
+    #     if len(content['guessHistory'])!=5:
+    #         correct = random.choices(characters, k=5) 
+    #     else:
+    #         for j in range(5):
+    #             if correct[j]==0:
+    #                 if len(wrong_positions) == 0:
+    #                     correct[j]="".join(random.choices(wrong_positions,k=1))
+    #                 else:
+    #                     correct[j]="".join(random.choices(characters,k=1))
         
-        correct = "".join(correct)
-        guess={"guess":correct}
-        print(content)
-        print(guess)
-        print(characters)
-        print(wrong_positions)
-    
+    #     correct = "".join(correct)
+    #     guess={"guess":correct}
+    #     print(content)
+    #     print(guess)
+    #     print(characters)
+    #     print(wrong_positions)
+    guess = {"guess":"choir"}
 
                 
                 
